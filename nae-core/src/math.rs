7185758d4335f518d625d3b3210a5bc41c5d2f22
Nazarí González <nazari.nz@gmail.com>
2019-12-22 00:36:45 +0000

    wip ported some code for the context2d

--- nae-core/src/math.rs
+++ nae-core/src/math.rs
@@ -1,6 +1,7 @@
-pub use nalgebra_glm::{
+pub use glm::{
     identity, mat3, mat4, rotation2d, scaling2d, translation2d, vec2, vec3, Mat3, Mat4, Vec2, Vec3,
 };
+use nalgebra_glm as glm;
 pub use std::f32::consts::PI;
 use std::ops::*;
 use std::sync::{Arc, Mutex};
@@ -10,4 +11,15 @@ use std::sync::{Arc, Mutex};
 pub fn eq_float(a: f32, b: f32) -> bool {
     //TODO improve this? https://floating-point-gui.de/errors/comparison/ it worth it? performance problems?
     (a - b).abs() < std::f32::EPSILON
+}
+
+pub fn projection_2d(width: i32, height: i32, flipped: bool) -> Mat3 {
+    let ww = width as f32;
+    let hh = height as f32;
+    let bottom = if flipped { 0.0 } else { hh };
+    let top = if flipped { hh } else { 0.0 };
+    glm::translate2d(
+        &glm::mat4_to_mat3(&glm::ortho(0.0, ww, bottom, top, -1.0, 1.0)),
+        &vec2(-ww * 0.5, -hh * 0.5),
+    )
 }
--- nae-glow/src/context.rs
+++ nae-glow/src/context.rs
@@ -1,71 +1,123 @@
-use crate::batchers::TextBatcher;
+use crate::batchers::{ColorBatcher, SpriteBatcher, TextBatcher};
 use crate::font::Font;
 use crate::shader::Shader;
 use crate::texture::Texture;
-use crate::{GlContext, Surface};
-use nae_core::graphics::{BaseContext2d, BlendMode, Color, Geometry, Transform2d, Vertex};
+use crate::{GlContext, GlowValue, Surface};
+use glow::HasContext;
+use glyph_brush::BrushAction::Draw;
+use nae_core::graphics::{
+    BaseContext2d, BaseShader, BlendFactor, BlendMode, Color, Geometry, Transform2d, Vertex,
+};
 use nae_core::math::*;
 use nae_core::resources::{BaseFont, HorizontalAlign, VerticalAlign};
+use std::rc::Rc;
 
-pub(crate) struct DrawData {
-    pub alpha: f32,
-    pub color: Color,
-    pub shader: Option<Shader>,
-    pub transform: Transform2d,
-    pub width: i32,
-    pub height: i32,
-    pub flipped: bool,
-    pub projection: Mat3,
-}
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::JsCast;
 
 pub struct Context2d {
     pub(crate) gl: GlContext,
     pub(crate) text_batcher: TextBatcher,
+    pub(crate) sprite_batcher: SpriteBatcher,
+    pub(crate) color_batcher: ColorBatcher,
+    is_drawing: bool,
+    data: DrawData,
+    paint_mode: PaintMode,
+    stencil: bool,
+    blend_mode: BlendMode,
+    is_drawing_surface: bool,
+    width: i32,
+    height: i32,
 }
 
+#[cfg(target_arch = "wasm32")]
+type Device = web_sys::HtmlCanvasElement;
+
+#[cfg(not(target_arch = "wasm32"))]
+type Device = ();
+
 impl BaseContext2d for Context2d {
+    type Device = Device;
     type Shader = Shader;
     type Surface = Surface;
     type Texture = Texture;
     type Font = Font;
 
-    fn set_shader(&mut self, shader: Option<&Self::Shader>) {
-        unimplemented!()
+    fn new(device: &Self::Device) -> Result<Self, String> {
+        create_context_2d(device)
     }
 
-    fn update_custom_shader(&mut self, shader: Option<&Self::Shader>) {
-        unimplemented!()
+    fn set_shader(&mut self, shader: Option<&Shader>) {
+        if shader.is_none() && self.data.shader.is_none() {
+            return;
+        }
+
+        if shader.is_some() && self.data.shader.is_none()
+            || shader.is_none() && self.data.shader.is_some()
+        {
+            self.update_custom_shader(shader);
+            return;
+        }
+
+        if let (Some(s1), Some(s2)) = (shader, &self.data.shader) {
+            if s1.is_equal(s2) {
+                self.update_custom_shader(shader);
+            }
+        }
+    }
+
+    fn update_custom_shader(&mut self, shader: Option<&Shader>) {
+        self.flush();
+        self.data.set_shader(shader);
+        if let Some(s) = shader {
+            s.use_me();
+        }
     }
 
     fn set_alpha(&mut self, alpha: f32) {
-        unimplemented!()
+        self.data.alpha = alpha;
     }
 
     fn set_blend(&mut self, mode: BlendMode) {
-        unimplemented!()
+        if mode != self.blend_mode {
+            self.flush();
+            self.blend_mode = mode;
+
+            unsafe {
+                self.gl.blend_func(
+                    self.blend_mode.source().glow_value(),
+                    self.blend_mode.destination().glow_value(),
+                );
+            }
+        }
     }
 
     fn set_size(&mut self, width: i32, height: i32) {
-        unimplemented!()
+        self.width = width;
+        self.height = height;
+
+        if !self.is_drawing {
+            self.data.set_size(width, height, false);
+        }
     }
 
     fn width(&self) -> i32 {
-        unimplemented!()
+        self.width
     }
 
     fn height(&self) -> i32 {
-        unimplemented!()
+        self.height
     }
 
     fn set_color(&mut self, color: Color) {
-        unimplemented!()
+        self.data.color = color;
     }
 
     fn transform(&mut self) -> &mut Transform2d {
-        unimplemented!()
+        &mut self.data.transform
     }
 
-    fn begin_to_surface(&mut self, surface: Option<&Self::Surface>) {
+    fn begin_to_surface(&mut self, surface: Option<&Surface>) {
         unimplemented!()
     }
 
@@ -260,5 +312,175 @@ impl BaseContext2d for Context2d {
         bottom: f32,
     ) {
         unimplemented!()
+    }
+}
+
+pub(crate) struct DrawData {
+    pub alpha: f32,
+    pub color: Color,
+    pub shader: Option<Shader>,
+    pub transform: Transform2d,
+    pub width: i32,
+    pub height: i32,
+    pub flipped: bool,
+    pub projection: Mat3,
+}
+
+impl DrawData {
+    pub fn new(width: i32, height: i32) -> Self {
+        let projection = projection_2d(width, height, false);
+        let transform = Transform2d::new();
+        Self {
+            width,
+            height,
+            alpha: 1.0,
+            shader: None,
+            transform,
+            color: Color::WHITE,
+            projection,
+            flipped: false,
+        }
+    }
+
+    pub fn set_size(&mut self, width: i32, height: i32, flipped: bool) {
+        if width != self.width || height != self.height || flipped != self.flipped {
+            self.upadte_projection(width, height, flipped);
+        }
+    }
+
+    pub fn set_shader(&mut self, shader: Option<&Shader>) {
+        self.shader = shader.map(|v| v.clone());
+    }
+
+    fn upadte_projection(&mut self, width: i32, height: i32, flipped: bool) {
+        self.width = width;
+        self.height = height;
+        self.flipped = flipped;
+        self.projection = projection_2d(self.width, self.height, flipped);
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum PaintMode {
+    Color,
+    Image,
+    Text,
+    Empty,
+}
+
+#[cfg(target_arch = "wasm32")]
+fn create_gl_context(win: &web_sys::HtmlCanvasElement) -> Result<(GlContext, String), String> {
+    if let Ok(ctx) = create_webgl2_context(win) {
+        return Ok((ctx, String::from("webgl2")));
+    }
+
+    let ctx = create_webgl_context(win)?;
+    Ok((ctx, String::from("webgl")))
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn create_gl_context(device: &()) -> Result<(GlContext, String), String> {
+    unimplemented!()
+}
+
+#[cfg(target_arch = "wasm32")]
+fn webgl_options() -> web_sys::WebGlContextAttributes {
+    let mut opts = web_sys::WebGlContextAttributes::new();
+    opts.stencil(true);
+    opts.premultiplied_alpha(false);
+    opts.alpha(false);
+    opts.antialias(true);
+    opts
+}
+
+#[cfg(target_arch = "wasm32")]
+fn create_webgl_context(win: &web_sys::HtmlCanvasElement) -> Result<GlContext, String> {
+    //TODO manage errors
+    let gl = win
+        .get_context_with_context_options("webgl", webgl_options().as_ref())
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::WebGlRenderingContext>()
+        .unwrap();
+
+    let ctx = Rc::new(glow::Context::from_webgl1_context(gl));
+    Ok(ctx)
+}
+
+#[cfg(target_arch = "wasm32")]
+fn create_webgl2_context(win: &web_sys::HtmlCanvasElement) -> Result<GlContext, String> {
+    //TODO manage errors
+    let gl = win
+        .get_context_with_context_options("webgl2", webgl_options().as_ref())
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::WebGl2RenderingContext>()
+        .unwrap();
+
+    let ctx = Rc::new(glow::Context::from_webgl2_context(gl));
+    Ok(ctx)
+}
+
+#[cfg(target_arch = "wasm32")]
+fn create_context_2d(win: &web_sys::HtmlCanvasElement) -> Result<Context2d, String> {
+    let width = win.width() as _;
+    let height = win.height() as _;
+    let (gl, driver) = create_gl_context(win)?;
+    let data = DrawData::new(width, height);
+    let blend_mode = BlendMode::NORMAL;
+
+    let text_batcher = TextBatcher::new(&gl)?;
+    let sprite_batcher = SpriteBatcher::new(&gl)?;
+    let color_batcher = ColorBatcher::new(&gl)?;
+
+    initialize_gl_2d(&gl, blend_mode);
+
+    Ok(Context2d {
+        data,
+        gl,
+        text_batcher,
+        sprite_batcher,
+        color_batcher,
+        blend_mode,
+        is_drawing: false,
+        is_drawing_surface: false,
+        paint_mode: PaintMode::Empty,
+        stencil: false,
+        width,
+        height,
+    })
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn create_context_2d(win: &()) -> Result<Context2d, String> {
+    unimplemented!()
+}
+
+fn initialize_gl_2d(gl: &GlContext, blend_mode: BlendMode) {
+    unsafe {
+        gl.disable(glow::DEPTH_TEST);
+        gl.enable(glow::BLEND);
+        gl.blend_func(
+            blend_mode.source().glow_value(),
+            blend_mode.destination().glow_value(),
+        );
+    }
+}
+
+impl GlowValue for BlendFactor {
+    fn glow_value(&self) -> u32 {
+        use BlendFactor::*;
+        match self {
+            One => glow::ONE,
+            Zero => glow::ZERO,
+            SourceColor => glow::SRC_COLOR,
+            InverseSourceColor => glow::ONE_MINUS_SRC_COLOR,
+            DestinationColor => glow::DST_COLOR,
+            InverseDestinationColor => glow::ONE_MINUS_DST_COLOR,
+            SourceAlpha => glow::SRC_ALPHA,
+            InverseSourceAlpha => glow::ONE_MINUS_SRC_ALPHA,
+            DestinationAlpha => glow::DST_ALPHA,
+            InverseDestinationAlpha => glow::ONE_MINUS_DST_ALPHA,
+        }
     }
 }
