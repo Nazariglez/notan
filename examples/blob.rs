5939cf2943143446d56f61615fbc29b71320b69e
Nazarí González <nazari.nz@gmail.com>
2019-12-28 23:39:22 +0000

    added a blob example

---
+++ examples/blob.rs
@@ -0,0 +1,29 @@
+use nae::prelude::*;
+
+fn init(app: &mut App) -> Blob {
+    app.load_file("../assets/blob.txt").unwrap()
+}
+
+fn draw(app: &mut App, blob: &mut Blob) {
+    let draw = app.draw();
+    draw.begin();
+    draw.clear(rgba(0.1, 0.2, 0.3, 1.0));
+    if blob.is_loaded() {
+        draw.text(&format!("Blob: {:?}", blob.data()), 10.0, 10.0, 24.0);
+        draw.text(
+            &format!(
+                "Text from blob: {:?}",
+                std::str::from_utf8(&blob.data()).unwrap()
+            ),
+            10.0,
+            60.0,
+            24.0,
+        );
+    }
+    draw.end();
+}
+
+#[nae::main]
+fn main() {
+    nae::with_state(init).draw(draw).build().unwrap();
+}
