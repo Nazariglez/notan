3fa059c1814317fd24096616137eb260fffc2474
Nazarí González <nazari.nz@gmail.com>
2019-12-29 08:15:13 +0000

    added panic hook on the web backend

--- nae-web/src/lib.rs
+++ nae-web/src/lib.rs
@@ -6,6 +6,7 @@ use nae_core::graphics::BaseContext2d;
 pub use nae_core::*;
 pub use nae_glow::*;
 pub use window::*;
+use std::panic;
 
 pub struct System {
     window: Window,
@@ -17,6 +18,7 @@ impl BaseSystem for System {
     type Context2d = Context2d;
 
     fn new(mut opts: BuilderOpts) -> Result<Self, String> {
+        panic::set_hook(Box::new(console_error_panic_hook::hook));
         let win = window::Window::new(&opts.title, opts.width, opts.height)?;
         let ctx2 = Context2d::new(&win.canvas)?;
         Ok(Self {
