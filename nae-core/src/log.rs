8a9afc60c7733c116e1cf3e82d9248ea0cb17180
Nazarí González <nazari.nz@gmail.com>
2019-12-31 15:59:45 +0000

    added a simple random and random_range generator on the math module

--- nae-core/src/log.rs
+++ nae-core/src/log.rs
@@ -1,11 +1,21 @@
 pub use ::log::{debug, error, info, trace, warn, Level};
 
 #[cfg(target_arch = "wasm32")]
-pub fn init(level: Level) {
+pub fn init_with_level(level: Level) {
     console_log::init_with_level(level);
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-pub fn init(level: Level) {
+pub fn init_with_level(level: Level) {
     simple_logger::init_with_level(level);
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn init() {
+    console_log::init_with_level(Level::Warn);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn init() {
+    simple_logger::init();
 }
--- nae-core/src/math.rs
+++ nae-core/src/math.rs
@@ -1,10 +1,39 @@
 pub use glm::{
     identity, mat3, mat4, rotation2d, scaling2d, translation2d, vec2, vec3, Mat3, Mat4, Vec2, Vec3,
 };
+use lazy_static::lazy_static;
 use nalgebra_glm as glm;
+use rand::distributions::uniform::{SampleBorrow, SampleUniform};
+use rand::distributions::{Distribution, Standard};
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg32;
 pub use std::f32::consts::PI;
 use std::ops::*;
 use std::sync::{Arc, Mutex};
+
+lazy_static! {
+    static ref RNG: Arc<Mutex<Pcg32>> = Arc::new(Mutex::new(Pcg32::from_entropy()));
+}
+
+pub fn random_reseed(seed: u64) {
+    *RNG.lock().unwrap() = Pcg32::seed_from_u64(seed);
+}
+
+pub fn random<T>() -> T
+where
+    Standard: Distribution<T>,
+{
+    RNG.lock().unwrap().gen()
+}
+
+pub fn random_range<T, B1, B2>(min: B1, max: B2) -> T
+where
+    T: SampleUniform,
+    B1: SampleBorrow<T> + Sized,
+    B2: SampleBorrow<T> + Sized,
+{
+    RNG.lock().unwrap().gen_range(min, max)
+}
 
 //TODO evluate replace all the vecs and mats with the `vek` crate? (SIMD = performace) (nalgebra is needed to collision libs?)
 
