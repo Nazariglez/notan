5b04ca5d273a518189d6c9f123ec502267c69be0
Nazarí González <nazari.nz@gmail.com>
2020-01-03 21:08:31 +0000

    improved transform example

--- examples/transform_extra.rs
+++ examples/transform_extra.rs
@@ -3,7 +3,7 @@ use nae::prelude::*;
 
 struct State {
     tex: Texture,
-    transform: Transform2d,
+    transforms: Vec<Transform2d>,
 }
 
 #[nae::main]
@@ -16,50 +16,82 @@ fn main() {
 }
 
 fn init(app: &mut App) -> State {
-    let mut transform = Transform2d::new(0.0, 0.0);
-    transform.set_anchor(0.5, 0.5).set_position(400.0, 300.0);
-
     State {
-        tex: app.load_file("./examples/assets/ferris.png").unwrap(),
-        transform: transform,
+        tex: app.load_file("./examples/assets/ferris_chef.png").unwrap(),
+        transforms: vec![],
     }
 }
 
+// Sets the size of the texture on the transform once is loaded
+fn update(app: &mut App, state: &mut State) {
+    if state.tex.is_loaded() && state.transforms.len() == 0 {
+        state.transforms = init_transforms(&state.tex);
+    }
+}
+
+fn init_transforms(img: &Texture) -> Vec<Transform2d> {
+    let mut transforms: Vec<Transform2d> = (0..5)
+        .map(|_| Transform2d::new(img.width(), img.height()))
+        .collect();
+
+    transforms[0]
+        .set_pivot(1.0, 1.0)
+        .set_anchor(0.0, 0.0)
+        .set_position(0.0, 0.0);
+    transforms[1]
+        .set_pivot(0.0, 1.0)
+        .set_anchor(1.0, 0.0)
+        .set_position(800.0, 0.0);
+    transforms[2]
+        .set_pivot(0.5, 0.5)
+        .set_anchor(0.5, 0.5)
+        .set_position(400.0, 300.0);
+    transforms[3]
+        .set_pivot(1.0, 0.0)
+        .set_anchor(0.0, 1.0)
+        .set_position(0.0, 600.0);
+    transforms[4]
+        .set_pivot(0.0, 0.0)
+        .set_anchor(1.0, 1.0)
+        .set_position(800.0, 600.0);
+
+    transforms
+}
+
 fn draw(app: &mut App, state: &mut State) {
-    let img = &state.tex;
+    //    let img = &state.tex;
 
     let draw = app.draw();
     draw.begin();
     draw.clear(rgba(0.1, 0.2, 0.3, 1.0));
 
-    draw.push_matrix(&state.transform.matrix());
-    draw.image(img, 0.0, 0.0);
-    draw.pop_matrix();
+    draw.set_color(Color::WHITE);
+    for (i, t) in state.transforms.iter_mut().enumerate() {
+        draw.set_color(Color::WHITE);
 
-    draw.text(
-        &format!(
-            "pos: ({}, {})\nanchor: ({}, {})",
-            state.transform.x,
-            state.transform.y,
-            state.transform.anchor_x,
-            state.transform.anchor_y
-        ),
-        10.0,
-        10.0,
-        20.0,
-    );
+        draw.push_matrix(t.matrix());
+        draw.image(&mut state.tex, 0.0, 0.0);
+        draw.pop_matrix();
+
+        t.rotation += 0.2 * math::PI / 180.0;
+    }
+
+    //Debug info
+    draw.set_color(Color::BLACK);
+    for (i, t) in state.transforms.iter_mut().enumerate() {
+        draw.text_ext(
+            &format!(
+                "pos: ({}, {})\nanchor: ({}, {})\npivot: ({}, {})",
+                t.x, t.y, t.anchor_x, t.anchor_y, t.pivot_x, t.pivot_y,
+            ),
+            t.x + t.width * (0.5 - t.anchor_x),
+            t.y + t.height * (0.5 - t.anchor_y),
+            24.0,
+            HorizontalAlign::Center,
+            VerticalAlign::Center,
+            None,
+        );
+    }
 
     draw.end();
-}
-
-// Sets the size of the texture on the transform once is loaded
-fn update(app: &mut App, state: &mut State) {
-    if state.transform.width != 0.0 {
-        return;
-    }
-
-    if state.tex.is_loaded() {
-        state.transform.width = state.tex.width();
-        state.transform.height = state.tex.height();
-    }
 }
