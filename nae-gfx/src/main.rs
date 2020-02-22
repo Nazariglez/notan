3849c852041d30ce4f4a999e8adf6514d79166f0
Nazarí González <nazari.nz@gmail.com>
2020-02-22 23:48:14 +0000

    minor update

--- nae-gfx/src/main.rs
+++ nae-gfx/src/main.rs
@@ -1,6 +1,13 @@
 use glow::{Context, HasContext};
 use nae_core::{BlendFactor, BlendMode, Color};
 use std::rc::Rc;
+use glutin::event_loop::ControlFlow;
+use glutin::event::{Event, WindowEvent};
+use crate::shader::{Shader, Driver};
+use std::cell::Ref;
+//Sample texture array limit https://stackoverflow.com/questions/20836102/how-many-textures-can-i-use-in-a-webgl-fragment-shader
+
+mod shader;
 
 pub(crate) type GlContext = Rc<Context>;
 
@@ -13,118 +20,84 @@ pub trait BaseGraphics {
     // etc...
 }
 
-struct Graphics {
+pub struct Graphics {
     pub(crate) gl: GlContext,
+    pub(crate) driver: Driver,
 }
 
-#[macro_export]
-macro_rules! include_glsl {
-    ($path:expr) => {{
-        let source = include_str!($path);
-        source.as_bytes()
-    }};
+impl Graphics {
+    pub fn new(gl: GlContext) -> Self {
+        Self {
+            gl,
+            driver: Driver::OpenGl3_3,
+        }
+    }
+}
+
+pub struct Draw2d<'gfx> {
+    gfx: Ref<'gfx, Graphics>,
 }
 
 fn main() {
-    let spv = read_spirv(Cursor::new(
-        &include_bytes!("../resources/shaders/color.vert.spv")[..],
-    ))
-    .unwrap();
-    let wgl = compile_spirv_to_glsl(&spv, Driver::WebGl).unwrap();
-    let wgl2 = compile_spirv_to_glsl(&spv, Driver::WebGl2).unwrap();
-    let gles2 = compile_spirv_to_glsl(&spv, Driver::OpenGlEs2_0).unwrap();
-    let gl3 = compile_spirv_to_glsl(&spv, Driver::OpenGl3_3).unwrap();
-    println!("wgl: {}\n", wgl);
-    println!("wgl2: {}\n", wgl2);
-    println!("gles2: {}\n", gles2);
-    println!("gl3: {}\n", gl3);
-    //    let n = include_glsl!("./color.vert.glsl");
-    //    println!("{:?}", n);
-}
+    let (gl, event_loop, windowed_context, shader_version) = unsafe {
+        let el = glutin::event_loop::EventLoop::new();
+        let wb = glutin::window::WindowBuilder::new()
+            .with_title("Hello triangle!")
+            .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+        let windowed_context = glutin::ContextBuilder::new()
+            .with_vsync(true)
+            .build_windowed(wb, &el)
+            .unwrap();
+        let windowed_context = windowed_context.make_current().unwrap();
+        let context = glow::Context::from_loader_function(|s| {
+            windowed_context.get_proc_address(s) as *const _
+        });
+        (context, el, windowed_context, "#version 410")
+    };
 
-use spirv_cross::{glsl, spirv, ErrorCode};
+    let gl = Rc::new(gl);
 
-fn compile_spirv_to_glsl(source: &[u32], driver: Driver) -> Result<String, String> {
-    let module = spirv::Module::from_words(source);
-    let mut ast = spirv::Ast::<glsl::Target>::parse(&module).map_err(error_code_to_string)?;
+    let g = Graphics::new(gl);
 
-    ast.set_compiler_options(&glsl::CompilerOptions {
-        version: driver.to_glsl_version().ok_or("Invalid driver version")?,
-        vertex: glsl::CompilerVertexOptions::default(),
-    })
-    .map_err(error_code_to_string)?;
+    let shader = Shader::new(
+        include_bytes!("../resources/shaders/color.vert.spv"),
+        include_bytes!("../resources/shaders/color.frag.spv"),
+        &g,
+    ).unwrap();
 
-    ast.compile().map_err(error_code_to_string)
-}
 
-fn error_code_to_string(err: ErrorCode) -> String {
-    match err {
-        ErrorCode::Unhandled => String::from("Unhandled"),
-        ErrorCode::CompilationError(e) => e,
-    }
-}
 
-#[derive(Clone, Copy)]
-enum Driver {
-    WebGl,
-    WebGl2,
-    OpenGl3_3,
-    OpenGlEs2_0,
-}
-
-impl Driver {
-    fn to_glsl_version(&self) -> Option<glsl::Version> {
-        use glsl::Version::*;
-        use Driver::*;
-
-        Some(match self {
-            WebGl => V1_00Es,
-            WebGl2 => V3_00Es,
-            OpenGl3_3 => V3_30,
-            OpenGlEs2_0 => V1_00Es,
-            _ => return None,
-        })
-    }
-}
-
-use std::io::Cursor;
-use std::{io, slice};
-
-// FUNCTION TAKED FROM GFX
-pub fn read_spirv<R: io::Read + io::Seek>(mut x: R) -> io::Result<Vec<u32>> {
-    let size = x.seek(io::SeekFrom::End(0))?;
-    if size % 4 != 0 {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "input length not divisible by 4",
-        ));
-    }
-    if size > usize::max_value() as u64 {
-        return Err(io::Error::new(io::ErrorKind::InvalidData, "input too long"));
-    }
-    let words = (size / 4) as usize;
-    let mut result = Vec::<u32>::with_capacity(words);
-    x.seek(io::SeekFrom::Start(0))?;
-    unsafe {
-        // Writing all bytes through a pointer with less strict alignment when our type has no
-        // invalid bitpatterns is safe.
-        x.read_exact(slice::from_raw_parts_mut(
-            result.as_mut_ptr() as *mut u8,
-            words * 4,
-        ))?;
-        result.set_len(words);
-    }
-    const MAGIC_NUMBER: u32 = 0x07230203;
-    if result.len() > 0 && result[0] == MAGIC_NUMBER.swap_bytes() {
-        for word in &mut result {
-            *word = word.swap_bytes();
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::LoopDestroyed => {
+                return;
+            }
+            Event::MainEventsCleared => {
+                windowed_context.window().request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    g.gl.clear(glow::COLOR_BUFFER_BIT);
+                    g.gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                }
+                windowed_context.swap_buffers().unwrap();
+            }
+            Event::WindowEvent { ref event, .. } => match event {
+                WindowEvent::Resized(physical_size) => {
+                    windowed_context.resize(*physical_size);
+                }
+                WindowEvent::CloseRequested => {
+                    // unsafe {
+                    //     gl.delete_program(program);
+                    //     gl.delete_vertex_array(vertex_array);
+                    // }
+                    *control_flow = ControlFlow::Exit
+                }
+                _ => (),
+            },
+            _ => (),
         }
-    }
-    if result.len() == 0 || result[0] != MAGIC_NUMBER {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "input missing SPIR-V magic number",
-        ));
-    }
-    Ok(result)
+    });
 }
+
