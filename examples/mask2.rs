35b92ae932f6390e2d39cca37e2829c4db8aca0a
Nazarí González <nazari.nz@gmail.com>
2020-05-04 01:19:19 +0100

    wip: added support for stencil on gfx and mask on graphics

--- examples/mask2.rs
+++ examples/mask2.rs
@@ -1,29 +1,28 @@
 use nae::prelude::*;
 
 struct State {
-    image: Texture,
+    image: nae_gfx::texture::Texture,
     offset: f32,
 }
 
 fn init(app: &mut App) -> State {
     State {
-        image: app.load_file("./examples/assets/t.png").unwrap(),
+        image: nae_gfx::texture::Texture::from_bytes(app, include_bytes!("assets/t.png")).unwrap(),
         offset: 0.0,
     }
 }
 
 fn draw(app: &mut App, state: &mut State) {
-    let draw = app.draw();
-    draw.begin();
-    draw.clear(Color::new(0.1, 0.2, 0.3, 1.0));
+    let draw = app.draw2();
+    draw.begin(Color::new(0.1, 0.2, 0.3, 1.0));
 
     //Mask
-    draw.begin_mask();
-    draw.stroke_triangle(400.0, 100.0, 100.0, 500.0, 700.0, 500.0, 20.0);
-    draw.circle(400.0, 350.0, 30.0);
-    draw.stroke_circle(400.0, 350.0, 50.0, 10.0);
-    draw.stroke_circle(400.0, 350.0, 70.0, 10.0);
-    draw.end_mask();
+    draw.mask(|draw| {
+        draw.stroke_triangle(400.0, 100.0, 100.0, 500.0, 700.0, 500.0, 20.0);
+        draw.circle(400.0, 350.0, 30.0);
+        draw.stroke_circle(400.0, 350.0, 50.0, 10.0);
+        draw.stroke_circle(400.0, 350.0, 70.0, 10.0);
+    });
 
     //Draw on the mask
     draw.pattern(
@@ -35,6 +34,7 @@ fn draw(app: &mut App, state: &mut State) {
         state.offset,
         -state.offset,
     );
+
     draw.end();
 
     state.offset += 50.0 * app.delta;
