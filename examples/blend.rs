2bb43b1c4932d5499996bb138cef74b756182f8e
Nazarí González <nazari.nz@gmail.com>
2020-05-08 00:39:47 +0100

    fixed blend modes

--- examples/blend.rs
+++ examples/blend.rs
@@ -6,19 +6,18 @@ fn main() {
 }
 
 fn draw(app: &mut App, _: &mut ()) {
-    let draw = app.draw();
-    draw.begin();
-    draw.clear(Color::new(0.1, 0.2, 0.3, 1.0));
+    let draw = app.draw2();
+    draw.begin(Color::new(0.1, 0.2, 0.3, 1.0));
 
-    draw.set_blend(BlendMode::ADD);
+    draw.blend_mode = BlendMode::ADD;
 
-    draw.set_color(Color::GREEN);
+    draw.color = Color::GREEN;
     draw.circle(400.0, 225.0, 150.0);
 
-    draw.set_color(Color::RED);
+    draw.color = Color::RED;
     draw.circle(325.0, 375.0, 150.0);
 
-    draw.set_color(Color::BLUE);
+    draw.color = Color::BLUE;
     draw.circle(475.0, 375.0, 150.0);
 
     draw.end();
