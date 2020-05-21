73832c9c67c17a820e8f7a6ee017457688cb321f
Nazarí González <nazari.nz@gmail.com>
2020-05-22 00:06:21 +0100

    changes a little bit how the resource trait works

--- src/res/blob.rs
+++ src/res/blob.rs
@@ -2,7 +2,7 @@ use super::ResourceParser;
 use crate::app::App;
 use crate::resource_parser;
 use backend::{Draw, Graphics};
-use nae_core::{BaseApp, BaseSystem};
+use nae_core::{BaseApp, BaseSystem, Resource};
 use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 
@@ -24,30 +24,42 @@ impl Blob {
         RefMut::map(self.inner.borrow_mut(), |data| data)
     }
 
-    pub fn is_loaded(&self) -> bool {
-        self.inner.borrow().len() != 0
-    }
-
-    pub fn parse_data<T, S>(&mut self, app: &mut T, data: Vec<u8>) -> Result<(), String>
-    where
-        T: BaseApp<System = S>,
-        S: BaseSystem,
-    {
-        *self.inner.borrow_mut() = data;
-        Ok(())
-    }
-
     /// Create a new blob from bytes
     pub fn from_bytes<T, S>(app: &mut T, data: &[u8]) -> Result<Self, String>
     where
         T: BaseApp<System = S>,
-        S: BaseSystem<Graphics = Graphics, Draw = Draw>,
+        S: BaseSystem<Graphics = Graphics>,
     {
         let blob = Blob {
             inner: Rc::new(RefCell::new(data.to_vec())),
         };
 
         Ok(blob)
+    }
+}
+
+impl Resource for Blob {
+    type Graphics = Graphics;
+
+    fn new<T, S>(app: &mut T) -> Result<Self, String>
+    where
+        T: BaseApp<System=S>,
+        S: BaseSystem<Graphics=Self::Graphics>
+    {
+        Self::from_bytes(app, &[])
+    }
+
+    fn set_data<T, S>(&mut self, app: &mut T, data: Vec<u8>) -> Result<(), String>
+    where
+        T: BaseApp<System=S>,
+        S: BaseSystem<Graphics=Self::Graphics>
+    {
+        *self.inner.borrow_mut() = data;
+        Ok(())
+    }
+
+    fn is_loaded(&self) -> bool {
+        self.inner.borrow().len() != 0
     }
 }
 
