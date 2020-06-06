94b83b690e48ccc947ea773c3215490980cd8cc4
Nazarí González <nazari.nz@gmail.com>
2020-06-07 00:35:38 +0100

    added some utils to calculate world to local and local to world coordinates

--- nae-core/src/math.rs
+++ nae-core/src/math.rs
@@ -1,13 +1,4 @@
-use lazy_static::lazy_static;
-use rand::distributions::uniform::{SampleBorrow, SampleUniform};
-use rand::distributions::{Distribution, Standard};
-use rand::{Rng, SeedableRng};
-use rand_pcg::Pcg32;
-pub use std::f32::consts::PI;
-use std::ops::*;
-use std::sync::{Arc, Mutex};
-
-#[derive(Default, Clone)]
+#[derive(Default, Clone, Debug)]
 pub struct Rect {
     pub x: f32,
     pub y: f32,
