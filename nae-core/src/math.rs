3fb95d23ea7b75b6af24aea06e96f6810577e96f
Nazarí González <nazari.nz@gmail.com>
2020-06-07 22:42:02 +0100

    minor change

--- nae-core/src/math.rs
+++ nae-core/src/math.rs
@@ -1,3 +1,6 @@
+pub const DEG_TO_RAD: f32 = std::f32::consts::PI / 180.0;
+pub const RAD_TO_DEG: f32 = 180.0 / std::f32::consts::PI;
+
 #[derive(Default, Clone, Debug)]
 pub struct Rect {
     pub x: f32,
