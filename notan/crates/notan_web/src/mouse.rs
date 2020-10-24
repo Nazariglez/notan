2927e10f4dd955df999608d3bfd62784d793b054
Nazarí González <nazari.nz@gmail.com>
2020-10-24 09:13:38 +0100

    added mouse support

--- notan/crates/notan_web/src/mouse.rs
+++ notan/crates/notan_web/src/mouse.rs
@@ -5,16 +5,16 @@ use notan_app::Event;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use web_sys::{Event as WebEvent, MouseEvent};
+use web_sys::{Event as WebEvent, MouseEvent, WheelEvent};
 
 #[derive(Default)]
-pub struct MouseContext {
-    move_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
-    down_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
-    up_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
-    left_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
-    enter_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
-    wheel_callback_ref: Option<Closure<FnMut(MouseEvent)>>,
+pub struct MouseCallbacks {
+    on_move: Option<Closure<FnMut(MouseEvent)>>,
+    on_down: Option<Closure<FnMut(MouseEvent)>>,
+    on_up: Option<Closure<FnMut(MouseEvent)>>,
+    on_left_window: Option<Closure<FnMut(MouseEvent)>>,
+    on_enter_window: Option<Closure<FnMut(MouseEvent)>>,
+    on_wheel: Option<Closure<FnMut(WheelEvent)>>,
 }
 
 fn mouse_button_to_nae(btn: i16) -> MouseButton {
@@ -30,11 +30,11 @@ pub fn enable_mouse(
     win: &mut WebWindowBackend,
     fullscreen_dispatcher: Rc<RefCell<dyn Fn()>>,
 ) -> Result<(), String> {
-    let ctx = &mut win.mouse_context;
+    let callbacks = &mut win.mouse_callbacks;
 
     let events = win.events.clone();
     let canvas = win.canvas.clone();
-    ctx.move_callback_ref = Some(canvas_add_event_listener(
+    callbacks.on_move = Some(canvas_add_event_listener(
         &canvas.clone(),
         "mousemove",
         move |e: MouseEvent| {
@@ -45,18 +45,71 @@ pub fn enable_mouse(
 
     let events = win.events.clone();
     let canvas = win.canvas.clone();
-    ctx.down_callback_ref = Some(canvas_add_event_listener(
+    let fullscreen = fullscreen_dispatcher.clone();
+    callbacks.on_down = Some(canvas_add_event_listener(
         &canvas.clone(),
         "mousedown",
         move |e: MouseEvent| {
-            (*fullscreen_dispatcher.borrow())();
+            (*fullscreen.borrow())();
             let button = mouse_button_to_nae(e.button());
             let (x, y) = canvas_position_from_global(&canvas, e);
             events.borrow_mut().push(Event::MouseDown { button, x, y });
         },
     )?);
 
-    //TODO
+    let events = win.events.clone();
+    let canvas = win.canvas.clone();
+    let fullscreen = fullscreen_dispatcher.clone();
+    callbacks.on_up = Some(canvas_add_event_listener(
+        &canvas.clone(),
+        "mouseup",
+        move |e: MouseEvent| {
+            (*fullscreen.borrow())();
+            let button = mouse_button_to_nae(e.button());
+            let (x, y) = canvas_position_from_global(&canvas, e);
+            events.borrow_mut().push(Event::MouseUp { button, x, y });
+        },
+    )?);
+
+    let events = win.events.clone();
+    let canvas = win.canvas.clone();
+    let fullscreen = fullscreen_dispatcher.clone();
+    callbacks.on_left_window = Some(canvas_add_event_listener(
+        &canvas.clone(),
+        "mouseout",
+        move |e: MouseEvent| {
+            (*fullscreen.borrow())();
+            let (x, y) = canvas_position_from_global(&canvas, e);
+            events.borrow_mut().push(Event::MouseLeft { x, y });
+        },
+    )?);
+
+    let events = win.events.clone();
+    let canvas = win.canvas.clone();
+    let fullscreen = fullscreen_dispatcher.clone();
+    callbacks.on_enter_window = Some(canvas_add_event_listener(
+        &canvas.clone(),
+        "mouseover",
+        move |e: MouseEvent| {
+            (*fullscreen.borrow())();
+            let (x, y) = canvas_position_from_global(&canvas, e);
+            events.borrow_mut().push(Event::MouseEnter { x, y });
+        },
+    )?);
+
+    let events = win.events.clone();
+    let canvas = win.canvas.clone();
+    callbacks.on_wheel = Some(canvas_add_event_listener(
+        &canvas.clone(),
+        "wheel",
+        move |e: WheelEvent| {
+            let delta_x = e.delta_x() as _;
+            let delta_y = e.delta_y() as _;
+            events
+                .borrow_mut()
+                .push(Event::MouseWheel { delta_x, delta_y });
+        },
+    )?);
 
     Ok(())
 }
