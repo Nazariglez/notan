8d5c4c82858d484e777d744c1f1ed6faf09932ac
Nazarí González <nazari.nz@gmail.com>
2020-11-17 23:38:54 +0000

    removed unused imports

--- notan/crates/notan_app/src/assets.rs
+++ notan/crates/notan_app/src/assets.rs
@@ -1,18 +1,3 @@
-// use crate::app::App;
-// use downcast_rs::{impl_downcast, Downcast};
-// use futures::future::{BoxFuture, LocalBoxFuture};
-// use futures::prelude::*;
-// use futures::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-// use futures::Future;
-// use hashbrown::HashMap;
-// use indexmap::{IndexMap, IndexSet};
-// use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard};
-// use std::any::{Any, TypeId};
-// use std::ops::Deref;
-// use std::path::Path;
-// use std::sync::atomic::{AtomicBool, Ordering};
-// use std::sync::Arc;
-
 mod asset;
 mod bytes;
 mod list;
