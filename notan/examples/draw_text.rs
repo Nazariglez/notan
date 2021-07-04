344ee45df47437c2f28905e4727612909254b980
Nazarí González <nazari.nz@gmail.com>
2021-07-04 19:45:36 +0100

    added more examples

--- notan/examples/draw_text.rs
+++ notan/examples/draw_text.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), String> {
     notan::init_with(setup).draw(draw).build()
 }
 
-fn setup(app: &mut App, gfx: &mut Graphics) -> State {
+fn setup(gfx: &mut Graphics) -> State {
     let font = gfx
         .create_font(include_bytes!("./assets/Ubuntu-B.ttf"))
         .unwrap();
