688cbef25a2545ab06c82fe0e1f90385d2fa7efb
Nazarí González <nazari.nz@gmail.com>
2021-08-08 23:35:35 +0100

    minor improvements

--- notan/crates/notan_winit/src/backend.rs
+++ notan/crates/notan_winit/src/backend.rs
@@ -1,24 +1,28 @@
 use crate::window::WinitWindowBackend;
+use glutin::event::ElementState;
 use glutin::event_loop::ControlFlow;
 use notan_app::buffer::VertexAttr;
 use notan_app::commands::Commands;
 use notan_app::config::WindowConfig;
 use notan_app::graphics::pipeline::PipelineOptions;
+use notan_app::prelude::mouse::MouseButton;
 use notan_app::{
-    App, Backend, BackendSystem, DeviceBackend, EventIterator, InitializeFn, LoadFileFn,
+    App, Backend, BackendSystem, DeviceBackend, Event, EventIterator, InitializeFn, LoadFileFn,
     ResourceId, TextureInfo, TextureUpdate, WindowBackend,
 };
-use winit::event::{Event, WindowEvent};
+use winit::event::{Event as WEvent, WindowEvent};
 use winit::event_loop::EventLoop;
 
 pub struct WinitBackend {
     window: Option<WinitWindowBackend>,
+    events: EventIterator,
     exit_requested: bool,
 }
 
 impl WinitBackend {
     pub fn new() -> Result<Self, String> {
         Ok(Self {
+            events: EventIterator::new(),
             window: None,
             exit_requested: false,
         })
@@ -31,7 +35,7 @@ impl Backend for WinitBackend {
     }
 
     fn events_iter(&mut self) -> EventIterator {
-        Default::default()
+        self.events.take_events()
     }
 
     fn exit(&mut self) {
@@ -57,23 +61,51 @@ impl BackendSystem for WinitBackend {
         self.window = Some(WinitWindowBackend::new(window, &event_loop)?);
 
         Ok(Box::new(move |mut app: App, mut state: S, mut cb: R| {
+            let (mut mouse_x, mut mouse_y) = (0, 0);
+
             event_loop.run(move |event, _win_target, control_flow| {
+                let b = backend(&mut app);
                 match event {
-                    Event::WindowEvent { ref event, .. } => match event {
+                    WEvent::WindowEvent { ref event, .. } => match event {
                         WindowEvent::CloseRequested => {
                             app.exit();
                         }
+                        WindowEvent::Resized(size) => {
+                            b.events.push(Event::WindowResize {
+                                width: size.width as _,
+                                height: size.height as _,
+                            });
+                        }
+                        WindowEvent::MouseInput { state, button, .. } => {
+                            let evt = match state {
+                                // TODO
+                                ElementState::Pressed => Event::MouseDown {
+                                    button: MouseButton::Left,
+                                    x: mouse_x,
+                                    y: mouse_y,
+                                },
+                                _ => Event::MouseUp {
+                                    button: MouseButton::Left,
+                                    x: mouse_x,
+                                    y: mouse_y,
+                                },
+                            };
+                            b.events.push(evt);
+                        }
+                        WindowEvent::CursorMoved { position, .. } => {
+                            mouse_x = position.x as _;
+                            mouse_y = position.y as _;
+                            b.events.push(Event::MouseMove {
+                                x: mouse_x,
+                                y: mouse_y,
+                            });
+                        }
                         _ => {}
                     },
-                    Event::MainEventsCleared => {
-                        backend(&mut app)
-                            .window
-                            .as_mut()
-                            .unwrap()
-                            .window()
-                            .request_redraw();
+                    WEvent::MainEventsCleared => {
+                        b.window.as_mut().unwrap().window().request_redraw();
                     }
-                    Event::RedrawRequested(_) => {
+                    WEvent::RedrawRequested(_) => {
                         cb(&mut app, &mut state);
                         backend(&mut app).window.as_mut().unwrap().swap_buffers();
                     }
