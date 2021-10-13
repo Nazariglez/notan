a809481f18350c5de3e8065bce0aec0e6f1dea44
Nazarí González <nazari.nz@gmail.com>
2021-10-13 23:25:29 +0100

    added support to draw animations from a grid

--- notan/crates/notan_draw/src/images.rs
+++ notan/crates/notan_draw/src/images.rs
@@ -1,3 +1,4 @@
+mod animation;
 mod image;
 mod instanced;
 mod nine_slice;
@@ -6,6 +7,7 @@ mod painter;
 //pub use instanced::*;
 use crate::builder::DrawBuilder;
 use crate::draw::Draw;
+pub use animation::*;
 pub use image::*;
 pub use nine_slice::*;
 use notan_graphics::Texture;
@@ -15,6 +17,13 @@ pub(crate) use painter::*;
 pub trait DrawImages {
     fn image<'a>(&mut self, texture: &'a Texture) -> DrawBuilder<Image<'a>>;
     fn nine_slice<'a>(&mut self, texture: &'a Texture) -> DrawBuilder<NineSlice<'a>>;
+    fn animation_grid<'a>(
+        &mut self,
+        texture: &'a Texture,
+        cols: usize,
+        rows: usize,
+    ) -> DrawBuilder<ImageAnimation<'a>>;
+    fn animation_list<'a>(&mut self, list: &'a [&'a Texture]) -> DrawBuilder<ImageAnimation<'a>>;
     //fn instanced_image<'a>(&mut self, texture: &'a Texture) -> DrawBuilder<InstancedImage<'a>>;
 }
 
@@ -25,5 +34,18 @@ impl DrawImages for Draw {
 
     fn nine_slice<'a>(&mut self, texture: &'a Texture) -> DrawBuilder<NineSlice<'a>> {
         DrawBuilder::new(self, NineSlice::new(texture))
+    }
+
+    fn animation_grid<'a>(
+        &mut self,
+        texture: &'a Texture,
+        cols: usize,
+        rows: usize,
+    ) -> DrawBuilder<ImageAnimation<'a>> {
+        DrawBuilder::new(self, ImageAnimation::from_grid(texture, cols, rows))
+    }
+
+    fn animation_list<'a>(&mut self, list: &'a [&'a Texture]) -> DrawBuilder<ImageAnimation<'a>> {
+        DrawBuilder::new(self, ImageAnimation::from_list(list))
     }
 }
