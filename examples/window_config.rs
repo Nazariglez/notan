a11acfa89d49cf8d9ee6e96f8e1508dd08118b22
Nazarí González <nazari.nz@gmail.com>
2021-10-15 20:07:41 +0100

    minor change

--- examples/window_config.rs
+++ examples/window_config.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), String> {
     // Check the documentation for more options
     let window_config = WindowConfig::new()
         .title("Window Config Demo")
-        .size(1026, 800) // window's size
+        .size(1026, 600) // window's size
         .vsync() // enable vsync
         .resizable() // window can be resized
         .min_size(600, 400); // Set a minimum window size
