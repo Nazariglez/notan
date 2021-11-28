6671dece7917f8acdaa722a550c049ac3bb743f9
Nazarí González <nazari.nz@gmail.com>
2021-11-28 21:11:27 +0000

    wip: glyph extension

--- crates/notan_gly/src/cache.rs
+++ crates/notan_gly/src/cache.rs
@@ -1,4 +1,5 @@
 use notan_app::{Graphics, Texture, TextureFilter, TextureFormat};
+use notan_graphics::Device;
 
 pub struct Cache {
     texture: Texture,
@@ -6,11 +7,11 @@ pub struct Cache {
 
 impl Cache {
     pub fn new(
-        gfx: &mut Graphics,
+        device: &mut Device,
         texture_width: u32,
         texture_height: u32,
     ) -> Result<Cache, String> {
-        let texture = gfx
+        let texture = device
             .create_texture()
             .with_size(texture_width as _, texture_height as _)
             .with_filter(TextureFilter::Linear, TextureFilter::Linear)
@@ -22,7 +23,7 @@ impl Cache {
 
     pub fn update(
         &mut self,
-        gfx: &mut Graphics,
+        device: &mut Device,
         offset: [u16; 2],
         size: [u16; 2],
         data: &[u8],
@@ -30,7 +31,8 @@ impl Cache {
         let [ox, oy] = offset;
         let [w, h] = size;
 
-        gfx.update_texture(&mut self.texture)
+        device
+            .update_texture(&mut self.texture)
             .with_x_offset(ox as _)
             .with_y_offset(oy as _)
             .with_width(w as _)
