47ac4b73103178ab305619a551ea4380719df8de
Nazarí González <nazari.nz@gmail.com>
2021-12-12 22:55:08 +0000

    api improvements

--- examples/text_hello.rs
+++ examples/text_hello.rs
@@ -34,56 +34,13 @@ fn use_tt(tt: &TT) {
 fn draw(gfx: &mut Graphics, state: &mut State) {
     let (w, h) = gfx.size();
     let mut tt = TT::new(w as _, h as _);
+
     tt.add_text(&state.s)
         .position(30.0, 30.0)
         .color(Color::RED)
         .size(30.0);
-    // .position(30.0, 30.0);
 
-    // tt.chain_text("text")
-    //
-    // tt.position(30.0, 30.0);
-    //
-    // tt.add_text("Hello text!");
-    // tt.add_text("More about hello world?");
-    // // tt.position(60.0, 90.0);
-    // tt.add_text("another text...");
-    //
+    tt.chain_text(" SUP!").size(50.0).color(Color::BLUE);
+
     gfx.render(&tt);
-    // use_tt(&tt);
-
-    // // Queue sections to draw
-    // state.glyph_brush.queue(
-    //     Section::new()
-    //         .with_screen_position((30.0, 30.0))
-    //         .with_bounds((width as _, height as _))
-    //         .add_text(
-    //             Text::default()
-    //                 .with_text("Hello notan_glyph!")
-    //                 .with_color([1.0, 0.0, 0.0, 1.0])
-    //                 .with_scale(40.0),
-    //         ),
-    // );
-    //
-    // state.glyph_brush.queue(
-    //     Section::new()
-    //         .with_screen_position((30.0, 90.0))
-    //         .with_bounds((width as _, height as _))
-    //         .add_text(
-    //             Text::default()
-    //                 .with_text("Hello notan_glyph!")
-    //                 .with_color([1.0, 1.0, 1.0, 1.0])
-    //                 .with_scale(40.0),
-    //         ),
-    // );
-    //
-    // // process the queue and return a renderer to draw
-    // let renderer = state
-    //     .glyph_brush
-    //     .create_renderer(&mut state.pipeline)
-    //     .clear(ClearOptions::color(Color::BLACK))
-    //     .process(gfx);
-    //
-    // // Draw the renderer to the screen
-    // gfx.render(&renderer);
 }
