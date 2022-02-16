8aa63f0d0834508ea510ddced48cc74ed38b2eb6
Nazariglez <nazari.nz@gmail.com>
2022-02-16 23:05:23 +0000

    updated wasm example generatio

--- scripts/web_example.sh
+++ scripts/web_example.sh
@@ -4,13 +4,15 @@ if [[ $2 == '--release' ]];
 then
   cargo build --target wasm32-unknown-unknown --release --example $1 --all-features
   wasm-bindgen ./target/wasm32-unknown-unknown/release/examples/$1.wasm --out-dir ./docs/examples/$1 --no-modules --browser
+  wasm-opt -O -o ./docs/examples/$1/$1_bg.wasm ./docs/examples/$1/$1_bg.wasm
+  gzip -f ./docs/examples/$1/$1_bg.wasm
 else
   cargo build --target wasm32-unknown-unknown --example $1 --all-features
   wasm-bindgen ./target/wasm32-unknown-unknown/debug/examples/$1.wasm --out-dir ./docs/examples/$1 --no-modules --browser --keep-debug --debug
 fi
 
 cp ./scripts/example.html ./docs/examples/$1.html
-index=$(sed "s/{{ EXAMPLE }}/${1}/g" "./scripts/example.html")
+index=$(sed "s/{{ EXAMPLE }}/${1}\.gz/g" "./scripts/example.html")
 echo "${index}" > ./docs/examples/$1.html
 
 if [[ $3 != '--no-assets' ]];
