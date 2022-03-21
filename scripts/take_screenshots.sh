f8c5bd645182503305b66970a937186c2f4097fb
Nazariglez <nazari.nz@gmail.com>
2022-03-21 19:56:39 +0000

    added a script to help getting the example screenshots

---
+++ scripts/take_screenshots.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this script needs `scrot` installed on a linux machine
+run() {
+  cargo run --example "$f" --all-features
+}
+
+finish() {
+  scrot "./docs/examples/images/$f.jpg" -u -d 30
+  path="target/debug/examples/$f"
+  p=$(ps -ef | awk -v path="$path" '$8==path {print $2}')
+  kill "$p"
+}
+
+mkdir -p ./docs/examples/images
+for f in ./examples/*.rs; do
+  f=$f
+  f=${f/\.\/examples\//""}
+  f=${f/.rs/""}
+  run "$f" &
+  finish "$f"
+done
+
