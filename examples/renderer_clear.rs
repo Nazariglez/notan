64a1dd3f1897411b92c02ac7c35603f5e81fee5d
Nazariglez <nazari.nz@gmail.com>
2022-03-26 21:09:32 +0000

    added an example about how to clear the screen

---
+++ examples/renderer_clear.rs
@@ -0,0 +1,25 @@
+use notan::prelude::*;
+
+#[notan_main]
+fn main() -> Result<(), String> {
+    notan::init().draw(draw).build()
+}
+
+fn draw(app: &mut App, gfx: &mut Graphics) {
+    // "Random" color bases on the app's time
+    let color = Color::from_rgb(
+        app.timer.time_since_init().cos(),
+        app.timer.time_since_init().sin(),
+        1.0,
+    );
+
+    // create a renderer object
+    let mut renderer = gfx.create_renderer();
+
+    // begin a pass to clear the screen
+    renderer.begin(Some(&ClearOptions::color(color)));
+    renderer.end();
+
+    // render to screen
+    gfx.render(&renderer);
+}
