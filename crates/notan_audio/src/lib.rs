b339771a74483ea3ca5455215495f192c2b08cdd
Nazariglez <nazari.nz@gmail.com>
2022-03-31 00:45:57 +0100

    wip rodio

--- crates/notan_audio/src/lib.rs
+++ crates/notan_audio/src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+/// Represent the audio implementation backend
+pub trait AudioBackend {
+    fn create_source(&mut self, info: &AudioSourceInfo) -> Result<u64, String>;
+    fn play(&mut self, source: u64, repeat: bool) -> Result<(), String>;
+}
+
+pub struct AudioManager {
+    backend: Box<dyn AudioBackend>,
+    // drop?
+}
+
+impl AudioManager {
+    pub fn new(backend: Box<dyn AudioBackend>) -> Result<Self, String> {
+        Ok(Self { backend })
     }
+
+    pub fn create_audio(&mut self, bytes: &[u8]) {
+        self.backend
+            .create_source(&AudioSourceInfo {
+                bytes: bytes.to_vec(),
+                typ: AudioFileType::Vorbis,
+            })
+            .unwrap();
+    }
+
+    pub fn play(&mut self, id: u64) {
+        self.backend.play(id, false).unwrap();
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialOrd, PartialEq, Hash)]
+pub enum AudioFileType {
+    Mp3,
+    Vorbis,
+    Flac,
+    Wav,
+}
+
+pub struct AudioSourceInfo {
+    pub bytes: Vec<u8>,
+    pub typ: AudioFileType,
 }
