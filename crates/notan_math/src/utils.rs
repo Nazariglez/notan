55dcb63212fab1ba3ecdc8578e7e5adc6d5a4183
Nazarí González <nazari.nz@gmail.com>
2022-08-13 21:28:38 +0100

    deprecated methods to calculate positions from screen to local because don't use the projection

--- crates/notan_math/src/utils.rs
+++ crates/notan_math/src/utils.rs
@@ -1,5 +1,7 @@
 /// Returns a local position from screen
 #[inline]
+#[deprecated]
+#[doc(hidden)]
 pub fn mat3_screen_to_local(x: f32, y: f32, m: glam::Mat3) -> glam::Vec2 {
     let inverse = m.inverse();
     let v = inverse * glam::vec3(x, y, 1.0);
@@ -8,6 +10,8 @@ pub fn mat3_screen_to_local(x: f32, y: f32, m: glam::Mat3) -> glam::Vec2 {
 
 /// Returns a screen position from local
 #[inline]
+#[deprecated]
+#[doc(hidden)]
 pub fn mat3_local_to_screen(x: f32, y: f32, m: glam::Mat3) -> glam::Vec2 {
     let v = m * glam::vec3(x, y, 1.0);
     glam::vec2(v.x, v.y)
@@ -15,6 +19,8 @@ pub fn mat3_local_to_screen(x: f32, y: f32, m: glam::Mat3) -> glam::Vec2 {
 
 /// Returns a local position from another local
 #[inline]
+#[deprecated]
+#[doc(hidden)]
 pub fn mat3_local_to_local(x: f32, y: f32, from: glam::Mat3, to: glam::Mat3) -> glam::Vec2 {
     let from_point = mat3_local_to_screen(x, y, from);
     mat3_screen_to_local(from_point.x, from_point.y, to)
