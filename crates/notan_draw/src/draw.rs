3462c05abe433ef38b098079f39de569b1233f80
Nazarí González <nazari.nz@gmail.com>
2022-12-03 11:22:17 +0000

    if a text batch has a alpha blening as none, set as over

--- crates/notan_draw/src/draw.rs
+++ crates/notan_draw/src/draw.rs
@@ -182,8 +182,16 @@ impl Draw {
                 BatchType::Text { .. } => &self.text_pipeline,
             };
 
+            // blending modes, by priority:
+            // 1. element draw
+            // 2. global draw blending
+            // 3. in some cases (like text), default mode
             let cbm = info.blend_mode().or(self.blend_mode);
-            let abm = info.alpha_mode().or(self.alpha_mode);
+            let abm = info.alpha_mode().or(self.alpha_mode).or_else(|| match typ {
+                // text is drawn from a RT we need to set Over alpha by default
+                BatchType::Text { .. } => Some(BlendMode::OVER),
+                _ => None,
+            });
 
             self.current_batch = Some(Batch {
                 typ: create_type(info),
@@ -259,7 +267,8 @@ impl Draw {
         self.add_batch(info, is_diff_type, create_type);
 
         if let Some(b) = &mut self.current_batch {
-            // vertices and indices are calculated before the flush to the gpu, so we need to store the text until that time
+            // vertices and indices are calculated before the flush to the gpu,
+            // so we need to store the text until that time
             if let BatchType::Text { texts } = &mut b.typ {
                 let global_matrix = *self.transform.matrix();
                 let matrix = match *info.transform() {
