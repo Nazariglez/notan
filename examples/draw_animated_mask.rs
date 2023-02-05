64de6c07e72c92bb73d276517e1a76ef835356de
Nazariglez <nazari.nz@gmail.com>
2023-02-05 21:24:38 +0000

    added new example draw_animated_mask.rs

---
+++ examples/draw_animated_mask.rs
@@ -0,0 +1,28 @@
+use notan::{draw::*, prelude::*};
+
+fn main() {
+    notan::init()
+        .add_config(WindowConfig::new().size(500, 500))
+        .add_config(DrawConfig)
+        .draw(|app: &mut App, gfx: &mut Graphics| {
+            let mut draw = gfx.create_draw();
+            draw.clear(Color::BLACK);
+
+            let elapsed = app.timer.time_since_init();
+            let pulse_progress = ((elapsed % 4.) - 2.).abs() / 2.; // 4s roundtrip pulse
+            let radius = 200. * (1. - pulse_progress);
+            dbg!(radius, pulse_progress);
+
+            let mut mask = gfx.create_draw();
+            mask.clear(Color::BLACK);
+            mask.circle(radius).position(250., 250.);
+
+            draw.mask(Some(&mask));
+            draw.rect((0., 0.), (500., 500.)).color(Color::GREEN);
+            draw.mask(None);
+
+            gfx.render(&draw);
+        })
+        .build()
+        .unwrap();
+}
