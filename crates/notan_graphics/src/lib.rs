f3d781d53d3ea9214a724777dc190577c80bfc3a
Nazariglez <nazari.nz@gmail.com>
2023-03-04 22:39:41 +0000

    Enabled compilation with --no-default-features excluding shader compilation macros

--- crates/notan_graphics/src/lib.rs
+++ crates/notan_graphics/src/lib.rs
@@ -22,7 +22,3 @@ pub use render_texture::*;
 pub use renderer::*;
 pub use shader::*;
 pub use texture::*;
-
-pub use notan_macro::{
-    fragment_shader, include_fragment_shader, include_vertex_shader, vertex_shader,
-};
