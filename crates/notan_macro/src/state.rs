3a1bdc1cd2cddb9ff94e66cd4063e2dd83316da0
Nazarí González <nazari.nz@gmail.com>
2023-05-31 23:12:57 +0100

    state allows to set lifetimes

--- crates/notan_macro/src/state.rs
+++ crates/notan_macro/src/state.rs
@@ -1,11 +1,15 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{Data, DataStruct, Fields};
 
 pub(crate) fn impl_state_derive(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
+    let generics = &ast.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
     let gen = quote! {
-        impl notan::app::AppState for #name {}
+        impl #impl_generics notan::app::AppState for #name #ty_generics #where_clause {}
     };
     gen.into()
 }
