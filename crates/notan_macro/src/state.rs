b095127fa4664d9544a919a031787fd843235667
Nazarí González <nazari.nz@gmail.com>
2023-06-04 21:45:08 +0100

    cargo fmt

--- crates/notan_macro/src/state.rs
+++ crates/notan_macro/src/state.rs
@@ -2,7 +2,6 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 
-
 pub(crate) fn impl_state_derive(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let generics = &ast.generics;
