0166f06f355dc3f7561027e0a4f806babd7bf3fc
Nazarí González <nazari.nz@gmail.com>
2023-07-23 18:53:31 +0100

    updated initial position example

--- examples/window_initial_position.rs
+++ examples/window_initial_position.rs
@@ -2,11 +2,6 @@ use notan::prelude::*;
 
 #[notan_main]
 fn main() -> Result<(), String> {
-    notan::init_with(init).build()
-}
-
-fn init(app: &mut App) {
-    // A position of (0, 0) puts the top-left of the window on the top-left of the main monitor.
-    // Increasing the first number (x position) moves the window to the right; increasing the second (y) moves it down. Decreasing does the opposite.
-    app.window().set_position(1000, 50);
+    let win = WindowConfig::default().set_position(100, 100);
+    notan::init().add_config(win).build()
 }
