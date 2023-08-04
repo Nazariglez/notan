354c39d0c58ec6c749c088440bc912f3a6991643
fand <mail@amagi.dev>
2023-08-04 16:03:42 -0700

    use Draw instead of Renderer

--- examples/draw_image_with_mipmaps.rs
+++ examples/draw_image_with_mipmaps.rs
@@ -36,10 +36,6 @@ struct State {
     pipeline: Pipeline,
     uniforms: Buffer,
 
-    pipeline2: Pipeline,
-    vertex_buffer: Buffer,
-    index_buffer: Buffer,
-
     is_first_render: bool,
 }
 
@@ -96,48 +92,6 @@ fn init(gfx: &mut Graphics) -> State {
         .build()
         .unwrap();
 
-    // Pipeline2
-    let vertex_info = VertexInfo::new()
-        .attr(0, VertexFormat::Float32x3)
-        .attr(1, VertexFormat::Float32x2);
-
-    #[rustfmt::skip]
-            let vertices = [
-                //pos               //coords
-                1.0,  1.0, 0.0,     1.0, 1.0,
-                1.0, -1.0, 0.0,     1.0, 0.0,
-                -1.0, -1.0, 0.0,    0.0, 0.0,
-                -1.0,  1.0, 0.0,    0.0, 1.0
-            ];
-
-    #[rustfmt::skip]
-            let indices = [
-                0, 1, 3,
-                1, 2, 3,
-            ];
-
-    let vertex_buffer = gfx
-        .create_vertex_buffer()
-        .with_info(&vertex_info)
-        .with_data(&vertices)
-        .build()
-        .unwrap();
-
-    let index_buffer = gfx
-        .create_index_buffer()
-        .with_data(&indices)
-        .build()
-        .unwrap();
-
-    let pipeline2 = gfx
-        .create_pipeline()
-        .from(&VERT, &FRAG)
-        .with_vertex_info(&vertex_info)
-        .with_color_blend(BlendMode::NONE)
-        .with_texture_location(0, "u_texture")
-        .build()
-        .unwrap();
-
     State {
         font,
         texture1,
@@ -147,68 +101,18 @@ fn init(gfx: &mut Graphics) -> State {
         pipeline,
         uniforms,
 
-        pipeline2,
-        vertex_buffer,
-        index_buffer,
-
         is_first_render: true,
     }
 }
 
-//language=glsl
-const VERT: ShaderSource = notan::vertex_shader! {
-    r#"
-    #version 450
-    layout(location = 0) in vec4 a_position;
-    layout(location = 1) in vec2 a_texcoord;
-    layout(location = 0) out vec2 v_texcoord;
-    void main() {
-        v_texcoord = a_texcoord;
-        gl_Position = vec4(a_position.x, a_position.y * -1.0, a_position.z, 1.0);
-    }
-    "#
-};
-
-//language=glsl
-const FRAG: ShaderSource = notan::fragment_shader! {
-    r#"
-    #version 450
-    layout(location = 0) in vec2 v_texcoord;
-    layout(location = 0) out vec4 outColor;
-    layout(binding = 0) uniform sampler2D u_texture;
-
-    void main() {
-        outColor = texture(u_texture, v_texcoord);
-    }
-    "#
-};
-
 fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
     if state.is_first_render {
         // Copy the original texture content to the RenderTextures
-        {
-            let mut renderer = gfx.device.create_renderer();
-
-            renderer.begin(Some(ClearOptions::color(Color::TRANSPARENT)));
-            renderer.set_pipeline(&state.pipeline2);
-            renderer.bind_texture_slot(0, 0, &state.texture1);
-            renderer.bind_buffers(&[&state.vertex_buffer, &state.index_buffer]);
-            renderer.draw(0, 6);
-            renderer.end();
-
-            gfx.render_to(&state.render_texture1, &renderer);
-        }
-        {
-            let mut renderer = gfx.device.create_renderer();
-
-            renderer.begin(Some(ClearOptions::color(Color::TRANSPARENT)));
-            renderer.set_pipeline(&state.pipeline2);
-            renderer.bind_texture_slot(0, 0, &state.texture1);
-            renderer.bind_buffers(&[&state.vertex_buffer, &state.index_buffer]);
-            renderer.draw(0, 6);
-            renderer.end();
-
-            gfx.render_to(&state.render_texture2, &renderer);
+        for rt in [&state.render_texture1, &state.render_texture2] {
+            let mut draw = gfx.create_draw();
+            draw.set_size(state.texture1.width(), state.texture1.height());
+            draw.image(&state.texture1).blend_mode(BlendMode::NONE);
+            gfx.render_to(rt, &draw);
         }
 
         state.is_first_render = false;
