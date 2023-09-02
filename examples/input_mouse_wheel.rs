e52e71a68bea1c99faaa2ec8fbbc0b8484d2e048
Nazarí González <nazari.nz@gmail.com>
2023-09-02 22:57:38 +0100

    simplified the input_mouse_wheel example

--- examples/input_mouse_wheel.rs
+++ examples/input_mouse_wheel.rs
@@ -14,7 +14,6 @@ fn main() -> Result<(), String> {
     notan::init_with(setup)
         .add_config(DrawConfig)
         .update(update)
-        .event(event)
         .draw(draw)
         .build()
 }
@@ -31,26 +30,14 @@ fn setup(gfx: &mut Graphics) -> State {
     }
 }
 
-fn event(state: &mut State, evt: Event) {
-    match evt {
-        Event::MouseWheel { delta_x, delta_y } => {
-            state.x = (state.x + delta_x).max(0.0).min(800.0);
-            state.y = (state.y + delta_y).max(0.0).min(600.0);
-        }
-        _ => {}
-    }
-}
-
 fn update(app: &mut App, state: &mut State) {
-    //Alt way of getting mouse scrolling
+    if app.mouse.is_scrolling() {
+        let delta_x = app.mouse.wheel_delta.x;
+        let delta_y = app.mouse.wheel_delta.y;
 
-    // if app.mouse.is_scrolling() {
-    //     let delta_x = app.mouse.wheel_delta.x;
-    //     let delta_y = app.mouse.wheel_delta.y;
-    //
-    //     state.x = (state.x + delta_x).max(0.0).min(800.0);
-    //     state.y = (state.y + delta_y).max(0.0).min(600.0);
-    // }
+        state.x = (state.x + delta_x).max(0.0).min(800.0);
+        state.y = (state.y + delta_y).max(0.0).min(600.0);
+    }
 }
 
 fn draw(gfx: &mut Graphics, state: &mut State) {
