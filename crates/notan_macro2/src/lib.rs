fe0f14312a2089a2a670a4d680b7019e1037d7ac
Nazarí González <nazari.nz@gmail.com>
2024-01-17 21:01:37 +0000

    New framework core (#300)
    Notan's new core

---
+++ crates/notan_macro2/src/lib.rs
@@ -0,0 +1,9 @@
+use proc_macro::*;
+
+mod state;
+
+#[proc_macro_derive(AppState)]
+pub fn state_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    state::impl_state_derive(&ast)
+}
