73c1ad1153765f8810134f310778dedb67cdf965
Nazarí González <nazari.nz@gmail.com>
2024-02-02 21:22:52 +0000

    added gfx crate

--- examples/window_multiple.rs
+++ examples/window_multiple.rs
@@ -12,12 +12,13 @@ fn main() -> Result<(), String> {
 
 fn open_windows(_: &InitEvent, app: &mut App) {
     (0..3).for_each(|n| {
-        let _ = app.create_window(
-            WindowConfig::default()
-                .with_title(&format!("Window {}", n))
-                .with_size(300 + n * 230, 300 + n * 200)
-                .with_position((100 + n * 150) as _, (100 + n * 130) as _),
-        )
-        .unwrap();
+        let _ = app
+            .create_window(
+                WindowConfig::default()
+                    .with_title(&format!("Window {}", n))
+                    .with_size(300 + n * 230, 300 + n * 200)
+                    .with_position((100 + n * 150) as _, (100 + n * 130) as _),
+            )
+            .unwrap();
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,7 @@ pub use notan_app2 as app;
 pub use notan_core as core;
 pub use notan_macro2 as macros;
 
+#[cfg(feature = "gfx")]
+pub use notan_gfx as gfx;
+
 pub use notan::*;
