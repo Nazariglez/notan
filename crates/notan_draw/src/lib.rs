d887fa4fdc8ca3aa599a58b56605018bd5183c00
Nazarí González <nazari.nz@gmail.com>
2024-02-20 14:03:04 +0000

    draw2d wip

--- crates/notan_draw/src/lib.rs
+++ crates/notan_draw/src/lib.rs
@@ -1,27 +1,3 @@
-mod batch;
-mod builder;
 mod config;
-mod custom_pipeline;
-mod draw;
-mod extension;
-mod images;
-mod manager;
-mod patterns;
-mod shapes;
-mod texts;
-mod transform;
-
-mod atlas;
-
-pub use atlas::*;
-pub use builder::*;
-pub use config::*;
-pub use custom_pipeline::*;
-pub use draw::*;
-pub use extension::*;
-pub use images::*;
-pub use manager::*;
-pub use patterns::*;
-pub use shapes::*;
-pub use texts::*;
-pub use transform::*;
+mod draw2d;
+mod renderer;
