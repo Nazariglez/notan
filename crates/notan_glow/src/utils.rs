7c7121a930ad9d048a8de4a62e3b413ccbaf199a
Nazari Gonzalez <nazari.nz@gmail.com>
2025-01-26 22:24:27 +0000

    udpated glow to latest version and deny fallback renderers on webgl

--- crates/notan_glow/src/utils.rs
+++ crates/notan_glow/src/utils.rs
@@ -23,6 +23,7 @@ fn webgl_options(antialias: bool, transparent: bool) -> web_sys::WebGlContextAtt
     opts.set_alpha(transparent);
     opts.set_antialias(antialias);
     opts.set_power_preference(web_sys::WebGlPowerPreference::HighPerformance);
+    opts.set_fail_if_major_performance_caveat(true);
     opts
 }
 
@@ -35,9 +36,9 @@ fn create_webgl_context(
     let gl = win
         .get_context_with_context_options("webgl", webgl_options(antialias, transparent).as_ref())
         .map_err(|e| format!("{e:?}"))?
-        .ok_or("Cannot adquire the Webgl context. Is the canvas already instantiated?")?
+        .ok_or("Cannot acquire the Webgl context. Is the canvas already instantiated?")?
         .dyn_into::<web_sys::WebGlRenderingContext>()
-        .map_err(|_| "Cannot adquire WebGL context.")?;
+        .map_err(|_| "Cannot acquire WebGL context.")?;
 
     let ctx = glow::Context::from_webgl1_context(gl);
     Ok(ctx)
@@ -52,9 +53,9 @@ fn create_webgl2_context(
     let gl = win
         .get_context_with_context_options("webgl2", webgl_options(antialias, transparent).as_ref())
         .map_err(|e| format!("{e:?}"))?
-        .ok_or("Cannot adquire the Webgl2 context. Is the canvas already instantiated?")?
+        .ok_or("Cannot acquire the Webgl2 context. Is the canvas already instantiated?")?
         .dyn_into::<web_sys::WebGl2RenderingContext>()
-        .map_err(|_| "Cannot adquire WebGL2 context.")?;
+        .map_err(|_| "Cannot acquire WebGL2 context.")?;
 
     let ctx = glow::Context::from_webgl2_context(gl);
     Ok(ctx)
